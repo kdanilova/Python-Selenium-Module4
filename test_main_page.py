11cea92ada2505b414f7eaaeadf7629b2129d2b4
Ksenia <danilova.ksenia@gmail.com>
2020-07-26 00:57:01 +0200

    added checks on login page, login form and register form in login_page.py. Added new test 'test_guest_can_see_login_page' to main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
-from selenium.webdriver.common.by import By
-
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
@@ -16,6 +15,12 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+
+def test_guest_can_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
 
 
 
