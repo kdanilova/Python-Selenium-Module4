35efad93b93098cd5ad0fa68edb70085823e0de6
Ksenia <danilova.ksenia@gmail.com>
2020-07-26 01:54:21 +0200

    added opening login page steps to test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page() # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -15,12 +17,6 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-
-def test_guest_can_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
 
 
 
