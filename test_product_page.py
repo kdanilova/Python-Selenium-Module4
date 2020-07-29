cc98c045c809720019d4e672f66ad154bcbebca9
Ksenia <danilova.ksenia@gmail.com>
2020-07-29 22:12:28 +0200

    added parametrization to test the adding product to basket from 10 pages. Added marking failed test as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,15 @@
+import pytest
 from pages.product_page import ProductPage
 
 
 #steps taken
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('number', ["0", "1", "2", "3", "4", "5", "6",
+                                    pytest.param("7", marks=pytest.mark.xfail),
+                                    "8", "9"])
+def test_guest_can_add_product_to_basket(browser, number):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}"
+    # link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)  # initialization of Page Object, driver and url address are passed to constructor
     page.open()  # open page
     page.should_be_add_to_basket()  # check if Add to basket button is presented
