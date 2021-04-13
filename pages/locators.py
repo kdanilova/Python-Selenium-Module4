337be44a324c613f3873e2a982a30500773194c6
Ksenia <danilova.ksenia@gmail.com>
2021-04-14 00:31:08 +0200

    moved login methods from main to base class.Added two cases on login to product page

--- pages/locators.py
+++ pages/locators.py
@@ -19,3 +19,8 @@ class ProductPageLocators():
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages :nth-child(1) .alertinner strong")
 
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,7 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
+
 
 
 #steps taken
@@ -45,3 +47,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.product_add_to_basket()
     page.should_message_disappeared() # message should disappear
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()  # open login page
+    login_page = LoginPage(browser, browser.current_url)  # initialization Login Page
+    login_page.should_be_login_page()
+
