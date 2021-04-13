93ca2144f7248c826126f7c2417c70afd1979fae
Ksenia <danilova.ksenia@gmail.com>
2021-04-14 00:40:55 +0200

    removed login methods from main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,11 +5,3 @@ class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
 
-#moved to base_page.py
-#class MainPage(BasePage):
-   # def go_to_login_page(self):
-        #login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        #login_link.click()
-
-    #def should_be_login_link(self):
-        #assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
