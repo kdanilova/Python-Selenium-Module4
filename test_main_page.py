11a667b27e77a14c1f0d73eac85f180dd69fd469
Ksenia <danilova.ksenia@gmail.com>
2021-04-17 00:55:15 +0200

    added pytest.mark for two test cases in test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,25 @@
+import pytest
+
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
-def gtest_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # initialization of Page Object, driver and url address are passed to constructor
-    page.open()  # open page
-    page.go_to_login_page()  # open login page
-    login_page = LoginPage(browser, browser.current_url)  # initialization Login Page
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)  # initialization of Page Object, driver and url address are passed to constructor
+        page.open()  # open page
+        page.go_to_login_page()  # open login page
+        login_page = LoginPage(browser, browser.current_url)  # initialization Login Page
+        login_page.should_be_login_page()
 
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
